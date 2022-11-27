ccde5f89c48b9fc0406e4207c55ccda70caf24f8
JeremyAyza <Ayzajeremyx7x@gmail.com>
2022-11-27 15:03:01 -0500

    create opciones agregar_producto

--- app/src/main/java/pe/idat/joeinventoryapp/Fragmentos/F_Todas.java
+++ app/src/main/java/pe/idat/joeinventoryapp/Fragmentos/F_Todas.java
@@ -1,5 +1,6 @@
 package pe.idat.joeinventoryapp.Fragmentos;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,17 +9,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import pe.idat.joeinventoryapp.OpcionesProducto.Agregar_Producto;
 import pe.idat.joeinventoryapp.R;
 
 
 public class F_Todas extends Fragment {
 
+    FloatingActionButton FAB_AgregarProducto;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_f__todas, container, false);
+        FAB_AgregarProducto=view.findViewById(R.id.FAB_AgregarProducto);
+        FAB_AgregarProducto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), Agregar_Producto.class));
+            }
+        });
+
         return view;
     }
 }
---
+++ app/src/main/java/pe/idat/joeinventoryapp/OpcionesProducto/Agregar_Producto.java
@@ -0,0 +1,16 @@
+package pe.idat.joeinventoryapp.OpcionesProducto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pe.idat.joeinventoryapp.R;
+
+public class Agregar_Producto extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_agregar_producto);
+  }
+}
